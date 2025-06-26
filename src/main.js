6201b47332b713bfb15543fce780a4f089de251a
Mhamad Sarkawt <mhaamad.sarkawt@gmail.com>
2025-06-26 13:38:34 +0300

    Initial commit
    Project scaffolding and initial file setup.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { registerFIBNativeBridge } from "@first-iraqi-bank/sdk/fib-native-bridge
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 registerFIBNativeBridge()
 app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
